dbba2343961cd030851c5dd66ecc62a6a61c1e13
rkorzen <korzeniewski@gmail.com>
2018-10-20 14:06:13 +0200

    Zad 11 - final

---
+++ zjazd_2/zad_11.py
@@ -0,0 +1,10 @@
+liczby = set()
+
+while True:
+    komenda = input("Wprowadź liczbę lub k by zakończyć: ")
+    if komenda == 'k':
+        break
+    liczba = int(komenda)
+    liczby.add(liczba)
+
+print(len(liczby & set(range(2, 101, 2))))
