73fd345de16f78592b7b2911ac943e47f2b520d4
rkorzen <korzeniewski@gmail.com>
2018-10-20 16:50:09 +0200

    zmiana w devel

--- zjazd_2/funkcje/zadanie_2.py
+++ zjazd_2/funkcje/zadanie_2.py
@@ -1,5 +1,5 @@
 def wiecej_niz(napis, prog):
-    return {znak for znak in napis if napis.lower().count(znak) > prog}
+    return { x for x in napis if napis.lower().count(x) > prog}
 
 if __name__ == "__main__":
     print(wiecej_niz("Ala ma kota", 2))
