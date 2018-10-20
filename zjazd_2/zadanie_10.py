704ec23addb4c59aad4a9544557efe7c22f2c9f7
rkorzen <korzeniewski@gmail.com>
2018-10-20 11:57:26 +0200

    Rozwiązanie sposób 1

--- zjazd_2/zadanie_10.py
+++ zjazd_2/zadanie_10.py
@@ -2,6 +2,11 @@ napis = input("Podaj napis: ")
 liczniki_liter = {}
 
 # zliczyć
+for litera in napis:
+    if litera in liczniki_liter:
+        liczniki_liter[litera] = liczniki_liter[litera] + 1
+    else:
+        liczniki_liter[litera] = 1
 
 # wyświetlić
 for litera in liczniki_liter:
