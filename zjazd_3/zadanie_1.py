41e0a0ce35aac71c59a104724c037fca444831f5
rkorzen <korzeniewski@gmail.com>
2018-11-03 13:05:52 +0100

    WIP: zjazd3

---
+++ zjazd_3/zadanie_1.py
@@ -0,0 +1,24 @@
+class Product(object):
+    def __init__(self, id, nazwa, cena):
+        self.ID = id
+        self.nazwa = nazwa
+        self.cena = cena
+
+    def print_info(self):
+        return f'Produkt "{self.nazwa}", id: {self.ID}, cena: {self.cena} PLN'
+
+
+def test_product():
+    product = Product(1, 'Woda', 10.99)
+
+    assert hasattr(product, "ID")
+    assert hasattr(product, "nazwa")
+    assert hasattr(product, "cena")
+
+    assert product.ID == 1
+    assert product.nazwa == "Woda"
+    assert product.cena == 10.99
+
+def test_product_print_info():
+    product = Product(1, 'Woda', 10.99)
+    assert product.print_info() == 'Produkt "Woda", id: 1, cena: 10.99 PLN'
---
+++ zjazd_3/zadanie_rozgrzewkowe.py
@@ -0,0 +1,34 @@
+"""
+Stwórz klasę `Dog` wedgług następującej specyfikacji:
+
+* pies zużywa energię szczekając (`bark`) i zyskuje śpiąc (`sleep`).
+* nowa instancja klasy `Dog` ma 10 jednostek energii
+* `Dog` ma metodę `sleep` która dodaje mu 2 jednostki energii
+* `Dog` ma metodę `bark` która konsumuje mu 1 jednostkę energii
+* `Dog` ma metodę `get_energy` która zwraca wartość energii instancji
+
+"""
+
+class Dog:
+
+    def __init__(self):
+        self.energy = 10
+
+    def get_energy(self):
+        return self.energy
+
+    def bark(self):
+        self.energy -= 1
+
+    def sleep(self):
+        self.energy += 2
+
+
+def test_dog():
+    dog = Dog()
+    assert dog.get_energy() == 10
+    dog.bark()
+    dog.bark()
+    assert dog.get_energy() == 8
+    dog.sleep()
+    assert dog.get_energy() == 10
